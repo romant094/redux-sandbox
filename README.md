f2b577eeede3e6c5d614b37e1c29b0b9b1d96537
Anton Romankov <romant094@gmail.com>
2019-04-19 17:01:15 +0300

    lesson 4

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ Created simple counter. Work in console.
 Now counter works with buttons +1 / -1.
 ## lesson 3
 Reducer function must be a pure function. To change state action object needs to have all params that influence on it. Added +random.
+## lesson 4
+Rewrote code in other way. Added action creator.
--- src/index.js
+++ src/index.js
@@ -16,17 +16,42 @@ const counter = (state = 0, action) => {
 
 const store = createStore(counter);
 
+const inc = () => {
+    return {
+        type: 'INC'
+    }
+};
+
+const dec = () => {
+    return {
+        type: 'DEC'
+    }
+};
+
+const rnd = (rndValue) => {
+    return {
+        type: 'RND',
+        rndValue
+    }
+};
+
 document
-    .querySelectorAll('.btn')
-    .forEach((btn) => {
-        btn.addEventListener('click', function () {
-            const id = this.getAttribute('id');
-            const rndValue = Math.floor(Math.random() * 10) + 1;
-            store.dispatch({
-                type: id.toUpperCase(),
-                rndValue
-            });
-        })
+    .querySelectorAll('.btn')[1]
+    .addEventListener('click', () => {
+        store.dispatch(inc());
+    });
+
+document
+    .querySelectorAll('.btn')[0]
+    .addEventListener('click', () => {
+        store.dispatch(dec());
+    });
+
+document
+    .querySelectorAll('.btn')[2]
+    .addEventListener('click', () => {
+        const rndValue = Math.floor(Math.random() * 10) + 1;
+        store.dispatch(rnd(rndValue));
     });
 
 const update = () => {
